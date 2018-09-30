716f2079c63b707fa5a904a0a18b3c131429c2d1
Mohit Kulkarni <mohit.kulkarni12@imperial.ac.uk>
2018-09-30 11:37:48 +0100

    SQ login working too

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -3,8 +3,7 @@
 //
 // const url = 'https://www.britishairways.com/travel/redeem/execclub/_gf/en_gb';
 //
-const username = '23386157'
-const pw = 'Test1234'
+
 //
 // const options = {
 //     uri: `https://www.britishairways.com/en-gb/home`,
@@ -25,14 +24,30 @@ const pw = 'Test1234'
 
 const puppeteer = require('puppeteer');
 const $ = require('cheerio');
-const url = 'https://www.britishairways.com/travel/redeem/execclub/_gf/en_gb';
+var sleep = require('sleep');
 
-const loginId = '#loginid'
-const password = '#password'
-const submit = '#navLoginForm > div:nth-child(5) > div > button'
+async function runBA() {
+    const username = '23386157'
+    const pw = 'Test1234'
+    const url = 'https://www.britishairways.com/travel/redeem/execclub/_gf/en_gb';
 
+    const loginId = '#loginid'
+    const password = '#password'
+    const submit = '#navLoginForm > div:nth-child(5) > div > button'
 
-async function run() {
+    const fromPicker = '#departurePoint'
+    const toPicker = '#destinationPoint'
+    const london = 'London, United Kingdom, LON, London (All Airports)'
+    const bombay = 'Mumbai, India, BOM, Chhatrapati Shivaji Intl'
+
+    const departPicker = '#departInputDate'
+    const returnPicker = '#returnInputDate'
+    const departDate = '#departInputDate_table > tbody > tr:nth-child(5) > td:nth-child(6) > div'
+    const returnDateNext = '#returnInputDate_root > div > div > div > div > div.picker__header > div.picker__nav--next'
+    const returnDate = '#returnInputDate_table > tbody > tr:nth-child(5) > td:nth-child(4) > div'
+
+    const getFlights = '#submitBtn'
+
     const browser = await puppeteer.launch({
         headless: false
     });
@@ -57,6 +72,68 @@ async function run() {
 
     await page.click(BUTTON_SELECTOR);
     await page.waitForNavigation();
+
+    await page.click(fromPicker)
+    await page.keyboard.type(london)
+
+    await page.click(toPicker)
+    await page.keyboard.type(bombay)
+
+    await page.click(departPicker)
+    await page.keyboard.type(departDate)
+
+    await page.click(returnPicker)
+    await page.click(returnDateNext)
+    await page.click(returnDate)
+
+    await page.click(getFlights)
+    await page.waitForNavigation();
+
 }
 
-run();
+async function runSQ() {
+    const url = 'https://www.singaporeair.com/kfLogin.form';
+
+    const username = '8814634208';
+    const pw = '376264';
+
+    const stupidFormDecline = 'body > div.insider-opt-in-notification > div > div:nth-child(3) > div.insider-opt-in-notification-button-container > div.insider-opt-in-notification-button.insider-opt-in-disallow-button'
+
+    const cookiesTag = '#container > aside.popup-1.popup--cookie.cookie-close.popup--cookie-continue'
+    const continueBtn = '#container > aside.popup-1.popup--cookie.cookie-close.popup--cookie-continue > div > div > a > div';
+
+    const browser = await puppeteer.launch({
+        headless: false
+    });
+
+    const page = await browser.newPage();
+
+    // await page.goto('https://github.com');
+    // await page.screenshot({ path: 'screenshots/github.png' });
+
+    await page.goto(url);
+
+    // dom element selectors
+    const USERNAME_SELECTOR = '#membership-1';
+    const PASSWORD_SELECTOR = '#membership-pin';
+    const BUTTON_SELECTOR = '#login';
+
+    await page.waitForSelector('body > div.insider-opt-in-notification');
+
+    await page.click(stupidFormDecline);
+
+    await page.click(USERNAME_SELECTOR);
+    await page.keyboard.type(username);
+
+    await page.waitForSelector(cookiesTag);
+    await page.click(continueBtn);
+
+    await page.click(PASSWORD_SELECTOR);
+    await page.keyboard.type(pw);
+
+    await page.click(BUTTON_SELECTOR);
+    await page.waitForNavigation();
+
+}
+
+runSQ();
